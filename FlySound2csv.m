ea5ba091910311e598c8ef15773fc15df55eb627
tony-azevedo <tazevedo@gmail.com>
2014-05-04 22:00:14 -0400

    adding .csv files

--- FlySound2csv.m
+++
@@ -1,8 +0,0 @@
-% Script to save a csv for each protocol
-
-% Find out what the date, fly, cell genotype are.
-% For each protocol in a data folder
-    
-    % get the trial stem and creation date for the file, 
-    % add to structure; date, fly, cell, genotype, trial name, time stapmp 
-    % run the structarr2csv
